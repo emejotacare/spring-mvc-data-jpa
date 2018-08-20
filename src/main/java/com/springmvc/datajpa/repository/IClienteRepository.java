0780fc628cc702bbc576c9099bcd43bdedb0ca41
María José <Mara Jos@DESKTOP-442GMF6>
2018-08-20 15:04:29 +0200

    Cambio CrudRepository por PagingAndSortingRepository en la interfaz del repositorio para incluir paginación

--- src/main/java/com/springmvc/datajpa/repository/IClienteRepository.java
+++ src/main/java/com/springmvc/datajpa/repository/IClienteRepository.java
@@ -3,7 +3,7 @@
  */
 package com.springmvc.datajpa.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.springmvc.datajpa.entity.Cliente;
 
@@ -16,8 +16,20 @@ import com.springmvc.datajpa.entity.Cliente;
  *         Cliente --> Entidad
  * 
  *         Long --> Tipo de dato de la primary key en la clase (entidad) Cliente
- *
  */
-public interface IClienteRepository extends CrudRepository<Cliente, Long> {
+
+/**
+ * public interface IClienteRepository extends CrudRepository<Cliente, Long> {
+ * 
+ * }
+ */
+
+/**
+ * PagingAndSortingRepository extiende de CrudRepository, con lo que se
+ * mantienen los métodos del crud (save,delete,findOne,findAll) y se incluyen
+ * además findAll(con sort para ordenar) y findAll (con Pageable para paginar)
+ * 
+ */
+public interface IClienteRepository extends PagingAndSortingRepository<Cliente, Long> {
 	
 }
